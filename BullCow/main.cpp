ee33bd954718f2d6516951a8687b2829fa881ed7
DwarfKhan <skmiscool@gmail.com>
2018-08-17 10:43:16 -0700

    Welcome Screen

---
+++ BullCow/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bull Cow" << endl;
+	cout << "Try to guess the " << WORD_LENGTH << " letter word!" << endl;
+	system("pause");
+	return 0;
+}
